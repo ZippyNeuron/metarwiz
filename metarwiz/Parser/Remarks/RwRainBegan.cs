f0a3561bc1e80876ddb46b5c4e69c5c18f7f2dbb
AndysRoom <andy.fitbit@gmx.com>
2021-08-29 20:09:00 +0100

    Implemented RAE and RAB metar items

---
+++ metarwiz/Parser/Remarks/RwRainBegan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ZippyNeuron.Metarwiz.Parser.Remarks
+{
+    public class RwRainBegan : RwMetarItem
+    {
+        private readonly string _rab;
+        private readonly string _minutepastthehour;
+        private readonly string _exacttime;
+        private readonly TimeSpan? _time;
+        private readonly int? _minutes;
+
+        public RwRainBegan(int position, string value) : base(position, value, Pattern)
+        {
+            _rab = Groups["RAB"].Value;
+            _minutepastthehour = Groups["MINUTEPASTTHEHOUR"].Value;
+            _exacttime = Groups["EXACTTIME"].Value;
+            _time = !String.IsNullOrEmpty(_exacttime) ? TimeSpan.ParseExact(_exacttime, "hhmm", null) : null;
+            _minutes = !String.IsNullOrEmpty(_minutepastthehour) ? int.Parse(_minutepastthehour) : null;
+        }
+
+        public TimeSpan? Time => _time;
+
+        public int? Minutes => _minutes;
+
+        public static string Pattern => @"^(?<RAB>RAB)((?<MINUTEPASTTHEHOUR>\d{2})|(?<EXACTTIME>\d{4}))$";
+
+        public static bool IsMatch(int position, string value) => Match(value, Pattern);
+
+        public override string ToString()
+        {
+            return String.Concat(
+                _rab,
+                (_time != null) ? _time?.ToString("hhmm") : String.Format("{0:00}", _minutes)
+            );
+        }
+    }
+}
---
+++ metarwiz/Parser/Remarks/RwRainEnded.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ZippyNeuron.Metarwiz.Parser.Remarks
+{
+    public class RwRainEnded : RwMetarItem
+    {
+        private readonly string _rae;
+        private readonly string _minutepastthehour;
+        private readonly string _exacttime;
+        private readonly TimeSpan? _time;
+        private readonly int? _minutes;
+
+        public RwRainEnded(int position, string value) : base(position, value, Pattern)
+        {
+            _rae = Groups["RAE"].Value;
+            _minutepastthehour = Groups["MINUTEPASTTHEHOUR"].Value;
+            _exacttime = Groups["EXACTTIME"].Value;
+            _time = !String.IsNullOrEmpty(_exacttime) ? TimeSpan.ParseExact(_exacttime, "hhmm", null) : null;
+            _minutes = !String.IsNullOrEmpty(_minutepastthehour) ? int.Parse(_minutepastthehour) : null;
+        }
+
+        public TimeSpan? Time => _time;
+
+        public int? Minutes => _minutes;
+
+        public static string Pattern => @"^(?<RAE>RAE)((?<MINUTEPASTTHEHOUR>\d{2})|(?<EXACTTIME>\d{4}))$";
+
+        public static bool IsMatch(int position, string value) => Match(value, Pattern);
+
+        public override string ToString()
+        {
+            return String.Concat(
+                _rae,
+                (_time != null) ? _time?.ToString("hhmm") : String.Format("{0:00}", _minutes)
+            );
+        }
+    }
+}
